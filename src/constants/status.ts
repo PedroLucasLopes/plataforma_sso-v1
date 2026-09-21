1f58d974833a896eee97e7d1171f8bcc50b42d16
PedroLucasLopes <pedrolopesparaguai@gmail.com>
2026-09-21 14:05:26 -0300

    fix translation

--- src/constants/status.ts
+++ src/constants/status.ts
@@ -24,7 +24,7 @@ function labelled<Key extends string>(
       {
         ...look,
         get label() {
-          return t(`status.${group}.${key}`) === null
+          return t(`status.${group}.${key}`)
             ? group
             : t(`status.${group}.${key}`);
         },
